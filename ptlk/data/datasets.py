9a6bfd03696a5a3fdc5027f0e2dc15856e986825
Edward <anqiyuanms@hotmail.com>
2021-12-11 16:43:56 -0600

    self.get_n_points()

--- ptlk/data/datasets.py
+++ ptlk/data/datasets.py
@@ -38,7 +38,7 @@ class Atrial(Dataset):
         self.all_examples, self.dirs = self.get_all_examples(dataset_path)
         labels_df = pd.read_csv(f'{dataset_path}/label.csv')
         self.filtered_df = labels_df[labels_df['Study number'].isin(self.dirs)]  # total 8 samples
-        # self.get_n_points()  # only need once
+        self.get_n_points()  # only need once
         self.template_id = 0  # select 0 as the template for inference
         if training:
             self.study_ids = self.filtered_df['Study number'].values[1:5]
@@ -55,8 +55,6 @@ class Atrial(Dataset):
         df = pd.read_csv(path)
         df = df.sample(n=405)
         points = torch.from_numpy(np.float32(df[['x_norm', 'y_norm', 'z_norm']].values))
-        # if self.transform:
-        #     points = self.transform(points)
 
         unipolar = torch.from_numpy(df['unipolar'].values)
         bipolar = torch.from_numpy(df['unipolar'].values)
@@ -77,6 +75,7 @@ class Atrial(Dataset):
         study_id = self.filtered_df['Study number'].values[0]
         path = f'{self.dataset_path}/Cleaned_PatientData/{study_id}/{study_id}_eam_data.csv'
         df = pd.read_csv(path)
+        df = df.sample(n=405)
         points = torch.from_numpy(
             np.float32(df[['x_norm', 'y_norm', 'z_norm']].values))
 
