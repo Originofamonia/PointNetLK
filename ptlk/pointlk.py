a9f3e23c80564737dc68599d7d4d38da40f67f62
Edward <anqiyuanms@hotmail.com>
2021-12-14 00:55:09 -0600

    print(self.g_series.size())

--- ptlk/pointlk.py
+++ ptlk/pointlk.py
@@ -152,6 +152,7 @@ class PointLK(torch.nn.Module):
 
         g = g0
         self.g_series = torch.zeros(maxiter + 1, *g0.size(), dtype=g0.dtype)
+        print(self.g_series.size())
         self.g_series[0] = g0.clone()
 
         if training:
