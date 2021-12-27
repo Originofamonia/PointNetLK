a7112f020c31a5581ca538ca6aba71dfc397739e
Edward <anqiyuanms@hotmail.com>
2021-12-26 23:04:44 -0600

    print(train_ids, test_ids)

--- experiments/atrial_classifier.py
+++ experiments/atrial_classifier.py
@@ -5,6 +5,7 @@ train a simple classifier on registered point cloud with uni/bi-polar
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import KFold, GridSearchCV
+from sklearn.metrics import precision_score, recall_score, f1_score
 import numpy as np
 
 
@@ -14,9 +15,12 @@ def main():
     """
     npzfile = np.load(f'saved_pt_10.npz')
     x0, x1, y0, y1 = npzfile['x0'], npzfile['x1'], npzfile['y0'], npzfile['y1']
-    x0 = np.reshape(x0, (x0.shape[0], -1))
-    x1 = np.reshape(x1, (x1.shape[0], -1))
-    print(x0)
+    x0 = np.reshape(x0, (x0.shape[0], -1))  # [8， -1]
+    x1 = np.reshape(x1, (x1.shape[0], -1))  # [8， -1]
+    kfold = KFold(n_splits=8, shuffle=True)
+    for fold, (train_ids, test_ids) in enumerate(kfold.split(X=x0, y=y0)):
+        # add classification here
+        print(train_ids, test_ids)
 
 
 if __name__ == '__main__':
