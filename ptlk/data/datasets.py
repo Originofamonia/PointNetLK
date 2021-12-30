790339fec10df59ccb0262cbe30f0d40d4e150e0
Edward <anqiyuanms@hotmail.com>
2021-12-30 15:14:28 -0600

    self.y = torch.from_numpy(y).long().unsqueeze(-1)

--- ptlk/data/datasets.py
+++ ptlk/data/datasets.py
@@ -30,7 +30,7 @@ class ModelNet(globset.Globset):
 class Voltages(Dataset):
     def __init__(self, x, y):
         self.x = torch.from_numpy(x).float()
-        self.y = torch.from_numpy(y).long()
+        self.y = torch.from_numpy(y).long().unsqueeze(-1)
 
     def __getitem__(self, idx):
         return self.x[idx], self.y[idx]
