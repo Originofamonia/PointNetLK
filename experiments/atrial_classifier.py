26b7da4cfad9925000c1c8e550a6b0f5a144da2b
Edward <anqiyuanms@hotmail.com>
2021-12-31 15:10:37 -0600

    x = x1

--- experiments/atrial_classifier.py
+++ experiments/atrial_classifier.py
@@ -141,8 +141,8 @@ def main():
 
     preds = []
     labels = []
-    for fold, (train_ids, test_ids) in enumerate(kfold.split(X=x0, y=y1)):
-        x = x0
+    for fold, (train_ids, test_ids) in enumerate(kfold.split(X=x1, y=y1)):
+        x = x1
         y = y1
         # svm_classifier(labels, preds, x, y, train_ids, test_ids)
         # lr_classifier(labels, preds, x, y, train_ids, test_ids)
