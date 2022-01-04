360b432ea1cbd8cfb0d773a45d3894b13b390b1a
Edward <anqiyuanms@hotmail.com>
2022-01-03 20:58:26 -0600

    df = df.sample(n=506

--- ptlk/data/datasets.py
+++ ptlk/data/datasets.py
@@ -78,7 +78,7 @@ class Atrial(Dataset):
         study_id = self.study_ids[idx]
         path = f'{self.dataset_path}/Cleaned_PatientData/{study_id}/{study_id}_eam_data.csv'
         df = pd.read_csv(path)
-        df = df.sample(n=406, replace=False, random_state=np.random.randint(444))
+        df = df.sample(n=506, replace=False, random_state=np.random.randint(444))
         points = torch.from_numpy(np.float32(df[['x_norm', 'y_norm', 'z_norm']].values))
 
         unipolar = torch.from_numpy(df['unipolar'].values)
